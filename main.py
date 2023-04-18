1eb4346a5bf84cc146c22ba21a618dc3ba5f97b8
MerkiZ <merkizd@yandex.ru>
2023-04-18 20:22:27 +0300

    rechanged

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from access import vk_token # Токены
 from tqdm import tqdm
 import datetime
 import json
+import sys
 
 
 class PartVK:
@@ -22,10 +23,14 @@ class PartVK:
         response = requests.get(
             'https://api.vk.com/method/users.get', params=params)
         res = response.json()
+        if not res['response']:
+            print('Пользователь не найден.')
+            sys.exit()
+        # pprint(res)
+        # sys.exit()
         need_id = res['response'][0]['id']
         return need_id
 
-    
     def get_photos(self): # Принимает ID - возвразщает json формат данных
         ids = self.get_info()
         params = {
@@ -39,38 +44,29 @@ class PartVK:
         response = requests.get(
             'https://api.vk.com/method/photos.get', params=params)
         res = response.json()
+        # pprint(res)
+        # sys.exit()
         return res['response']['items']
         
-
     def make_a_dict(self): # Функция получает json данные и перебирает их на составляющие, создавая словарь
         photos = self.get_photos()
-        sizes_str = 'wzyrqpoxms'
         pics_dict = {}
         for photo in tqdm(photos, desc='Создаем словарь... '):
-            time.sleep(0.5)
             likes = photo['likes']['count']  # Кол-во лайков
             timestamp = photo['date']  # Дата публикации в UNIX формате
             time_value = datetime.datetime.fromtimestamp(timestamp)
-            time_post = time_value.strftime('%d-%m-%Y %H:%M:%S')
-            if likes not in pics_dict:
-                file_name = likes
-            else:
-                file_name = f"{likes}/{time_post}"
-
-            max_size = 0
-            for size in tqdm(photo["sizes"], desc='Обработка фотографии...'):
-                time.sleep(0.5)
-                counter = 9
-                if size["height"] > 0:
-                    if size["height"] > max_size:
-                        pics_dict[file_name] = {'url':size['url'],'type':size['type']}
-                        max_size = size["height"]
-                else:
-                    if sizes_str.index(size["type"]) < counter:
-                        pics_dict[file_name] = {'url':size['url'],'type':size['type']}
-                        counter = sizes_str.index(size["type"])
+            time_post = time_value.strftime('%d-%m-%Y %H:%M:%S') # Дата в обычном формате
+            right_photo_size = sorted(photo['sizes'], key=lambda x: (x['width'], x['height']))[-1]
+            url = right_photo_size['url']
+            size_letter = right_photo_size['type']
+            
+            if likes in pics_dict:
+                likes = f'{likes}_{time_post}'
+                
+            pics_dict[likes] = {'url': url, 'size': size_letter}
+        # sys.exit()
         return pics_dict
-
+        
 
 class YandexDisk:
     def __init__(self, token, photo_list, folder_name):
@@ -84,20 +80,41 @@ class YandexDisk:
             'Authorization': 'OAuth {}'.format(self.ya_token)
         }
 
+    def __create_folder(self): # Приватный метод
+        href = 'https://cloud-api.yandex.net/v1/disk/resources/'
+        headers = self.get_headers()
+        params = {'path': self.folder_name}
+        response = requests.put(f'{href}', headers= headers, params= params)
+        if response.status_code == 201:
+            print('Папка создана')
+
     def upload_file_to_disk(self):  # Загружаем файлы на ЯДиск
         '''
         Метод позволяет загружать фото на ЯДиск по URL
 
         '''
+        self.__create_folder()
         href = 'https://cloud-api.yandex.net/v1/disk/resources/'
         method = 'upload'
         headers = self.get_headers()
-        for path_file, url_link in tqdm(self.photo_list.items(), desc='Загружаем фото на облако... '):
-            params = {'path': f"{self.folder_name}/{path_file}", 'url': url_link['url']}
+        json_list = []
+        for title, value in tqdm(self.photo_list.items(), desc='Загружаем фото на облако... '):
+            name = f'{title}.jpg'
+            size = value['size']
+            params = {'path': f"{self.folder_name}/{name}", 'url': value['url']}
             response = requests.post(f'{href}{method}/', params=params, headers=headers)
             response.raise_for_status()
-            time.sleep(0.5)
-        print()
+            # time.sleep(0.3)
+            json_list.append(
+                {'file_name': name, 
+                 'size': size}
+            )
+        with open('data.json', 'w') as jsonfile:
+            for json_dict in json_list:
+                json_str = json_dict
+                json_object = json.loads(json_str)
+                json.dump(json_object, jsonfile)
+        
         if response.status_code == 202:
             print("Фотографии успешно загружены!")
             print()
@@ -106,33 +123,14 @@ class YandexDisk:
             print()
 
 
-    def create_folder(self):
-        href = 'https://cloud-api.yandex.net/v1/disk/resources/'
-        headers = self.get_headers()
-        params = {'path': self.folder_name}
-        response = requests.put(f'{href}', headers= headers, params= params)
-        if response.status_code == 201:
-            print('Папка создана')
 
+    # def make_a_json(self):
+    #     with open('data.json', 'w') as jsonfile:
+    #         json.dump(self.photo_list, jsonfile)
+            
+    #     data = {}
 
-    def _get_upload_link(self):
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        headers = self.get_headers()
-        params = {"path": f'{self.folder_name}/Photos_dict', "overwrite": "true"}
-        response = requests.get(upload_url, headers=headers, params=params)
-        return response.json()
-
-
-    def upload_json_to_disk(self):
-        with open('data.json', 'w') as jsonfile:
-            json.dump(self.photo_list, jsonfile)
-        href = self._get_upload_link().get("href", "")
-        response = requests.put(url= href, data=open('data.json', 'rb'))
-        response.raise_for_status()
-        if response.status_code == 201:
-            print("Json-файл успешно загружен!")
-        else:
-            print("Ошибка загрузки Json-файла.")
+    #     for key, value in self.photo_list:
 
     
 if __name__ == "__main__":
@@ -144,8 +142,5 @@ if __name__ == "__main__":
     downloader_from_vk = PartVK(vk_token, person_id, count_photo)
     need_photo = downloader_from_vk.make_a_dict()
     
-    
     uploader_to_yadisk = YandexDisk(ya_token, need_photo, folder_name)
-    uploader_to_yadisk.create_folder()
     uploader_to_yadisk.upload_file_to_disk()
-    uploader_to_yadisk.upload_json_to_disk()
